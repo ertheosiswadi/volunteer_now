13890a3d85739d9a3c721418621248bbb256b04e
Ertheo Siswadi <ertheosiswadi@Ertheos-MacBook-Pro.local>
2019-01-31 20:04:40 -0800

    added index.js and installed some stuff

--- README.md
+++
@@ -1,2 +0,0 @@
-# volunteer_now
-project app that we're working on. volunteering made easy and fast
---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('./public'));
+
+app.listen(PORT, ()=>{
+	console.log('server is live');
+});
+
+app.get('/', (request, response)=>{
+	response.send("HELLO THERE");
+});
