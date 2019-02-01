a6a683fe4c319899337c323ac91ec2e6c05d960e
Ertheo Siswadi <ertheosiswadi@Ertheos-MacBook-Pro.local>
2019-01-31 23:58:44 -0800

    added stuff

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
+//--------- Initialize firestore
+const admin = require('firebase-admin');
+
+var serviceAccount = require('./serviceAccountKey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+var db = admin.firestore();
+//--------- Initialize firestore
+
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('./public'));
 
@@ -11,4 +23,26 @@ app.listen(PORT, ()=>{
 
 app.get('/', (request, response)=>{
 	response.send("HELLO THERE");
+	db.collection('users').get()
+	  .then((snapshot) => {
+	    snapshot.forEach((doc) => {
+	      console.log(doc.id, '=>', doc.data());
+	    });
+	  })
+	  .catch((err) => {
+	    console.log('Error getting documents', err);
+	  });
 });
+
+//--------- Firebase stuff
+
+var test_add = db.collection('users').doc('ertheo');
+
+var setTheo = test_add.set(
+{
+	'first' : 'Ertheo',
+	'second' : 'Siswadi',
+	'born' : {'month' : 'MAY', 'year': '2020'}
+});
+
+
